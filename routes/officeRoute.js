1a80402ab8bfe840f990938c1087bd394b3581f0
Niyodusenga Jean Bosco <bosco7209@gmail.com>
2019-02-06 16:24:35 +0200

    Applying airbnb style

--- routes/officeRoute.js
+++ routes/officeRoute.js
@@ -3,13 +3,8 @@ const officeController = require('../controllers/officeController');
 
 const route = express.Router();
 
-// Create political office record.
 route.post('/', officeController.createPoliticalOffice);
-
-// Fetch a political office record.
 route.get('/', officeController.getAllPoliticalOffices);
-
-// Fetch a specific political office record.
 route.get('/:id', officeController.getSpecificPoliticalOffice);
 
 module.exports = route;
--- routes/partyRoute.js
+++ routes/partyRoute.js
@@ -3,19 +3,10 @@ const partyController = require('../controllers/partyController');
 
 const route = express.Router();
 
-// Create a political party.
 route.post('/', partyController.createPoliticalPary);
-
-// Fetch all political parties records.
 route.get('/', partyController.getAllPoliticalPary);
-
-// Fetch a specific political party record.
 route.get('/:id', partyController.getSinglePoliticalParty);
-
-// Edit the name of a specific political party
 route.patch('/:id/name', partyController.editPoliticalPary);
-
-// Delete a specific political party
 route.delete('/:id', partyController.deletePoliticalPary);
 
 module.exports = route;
